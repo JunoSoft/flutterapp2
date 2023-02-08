349a90c8876d9675a5480053de51502e85707e46
Junosoft <junosoftltd@gmail.com>
2023-02-08 12:19:44 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,38 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
   const MyApp({super.key});
   @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp>{
+
+  var questionIndex = 0;
+  void answerQuestion(){
+    setState(() { 
+    questionIndex++;
+    });
+  }
+  @override
   Widget build(BuildContext context) {
+    var questions =["What do you want ?","What your favolite color","No more questions"];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My first App'),
         ),
         body: Column(
-          children:const [
-             Text('Who are you?'),
-           TextButton(onPressed: null, child:Text("HOME"))
+          children:[
+           Text(questions[questionIndex]),
+           ElevatedButton(onPressed:answerQuestion, child:const Text("Button 1")),
+           ElevatedButton(onPressed: answerQuestion, child:const Text("Button 2")),
+           ElevatedButton(onPressed: answerQuestion, child:const Text("Button 3")),
+           ElevatedButton(onPressed: answerQuestion, child:const Text("Button 4")),
           ],
         ),
       ),
