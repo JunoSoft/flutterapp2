ef36d7665212d0ef14c2c6bf3286a1f7c5475414
Junosoft <junosoftltd@gmail.com>
2023-02-08 21:09:44 +0200

    edit

--- lib/Question.dart
+++ lib/Question.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class Question extends StatelessWidget {
   final String question;
   const Question(this.question, {super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Text(question);
+  Widget build(BuildContext context){
+    // ignore: sized_box_for_whitespace
+    return Container(
+        width: double.infinity,
+        child: Text(
+          question,
+          style: const TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+        ));
   }
 }
